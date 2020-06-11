14ffdcf942ec37a62a525664cca113a25c703f59
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-06-11 17:12:30 +0200

    Added redux to header component(Login component)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import "./header.styles.scss";
+import {connect} from 'react-redux'
 
 import React from "react";
 import { Link } from "react-router-dom";
@@ -32,4 +33,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store'
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider sotre={store}> 
+    <Provider store={store}> 
       <BrowserRouter>
         <App />
       </BrowserRouter>
