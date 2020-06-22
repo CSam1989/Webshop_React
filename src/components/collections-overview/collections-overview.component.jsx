a60453acb923d56d0dab5dba1ecfe3514b9599c3
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-06-22 17:25:23 +0200

    Added collection page to redux

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,13 +1,14 @@
-import './collections-overview.styles.scss';
+import "./collections-overview.styles.scss";
 
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => 
+(
   <div className="collection-overview">
     {collections.map(({ id, ...otherCollectionprops }) => (
       <CollectionPreview key={id} {...otherCollectionprops} />
@@ -16,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
