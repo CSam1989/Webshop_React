31cd60113c62dfee38f107c7a77848b47f2473d1
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-06-26 10:32:03 +0200

    Added Stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GyCKcAULQjmmV2FNGCkVco4gIH1ZA7m2tqBRmiBEieLQjV9sPvJrIPL0aLrwjB25Xxv05ybkYLXVv1way86rsGV00nGRbjtdQ";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="React Webshop"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
