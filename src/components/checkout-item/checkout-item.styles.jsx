167eef1f54b0c52ffc5eec6df4eb230ce4060a8a
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-06-26 16:57:27 +0200

    Converted checkout item to styled components

---
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+const EqualWidth = css`
+  width: 23%;
+`;
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`;
+
+export const ImageContainer = styled.div`
+  ${EqualWidth}
+  padding-right: 15px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const TextContainer = styled.span`
+  ${EqualWidth}
+`;
+
+export const QuantityContainer = styled(TextContainer)`
+  display: flex;
+  span {
+    margin: 0 10px;
+  }
+  div {
+    cursor: pointer;
+  }
+`;
+
+export const RemoveButtonContainer = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`;
